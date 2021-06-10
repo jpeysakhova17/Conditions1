6e58febba9222389a6dcdc08d5f0a9c078033045
Jenny <jenny@elis-MacBook-Air.local>
2021-06-09 21:25:14 -0400

    done

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var actualTemp = 80;
+var desiredTemp = 60;
+
+if (actualTemp > desiredTemp) {
+    console.log ("Run A/C");
+} else if (actualTemp < desiredTemp) {
+    console.log ("Run Heat");
+} else {
+    console.log ("Standby");
+}
